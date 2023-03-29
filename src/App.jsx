7ea15b8f05a82c56210d5464ea5d699bf1b51767
JorgeTorquato <ellendel@gmail.com>
2023-03-29 14:59:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from './Sidebar';
+import Home from './Home';
+import Modal from './Modal';
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
+};
+export default App;
